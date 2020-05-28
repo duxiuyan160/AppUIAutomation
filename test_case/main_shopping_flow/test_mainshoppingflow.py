1a7bb97ef2e6d0dffd40e045a4c7df49fda31e81
duxiuyan <duxiuyan@126.com>
2020-05-28 13:55:15 +0800

    主购物流程测试用例

---
+++ test_case/main_shopping_flow/test_mainshoppingflow.py
@@ -0,0 +1,15 @@
+import pytest
+
+from appium import webdriver
+
+
+@pytest.mark.usefixtures("init_driver")
+class TestMainShoppingFlow:
+
+    def test_mainshoppingflow(self, init_driver):
+        """
+        执行用例：验证码登录->首页搜索商品->商品展示->商品加入购物车->结算->提交订单，并断言
+        :param init_driver:
+        :return:
+        """
+        assert "支付剩余时间" in init_driver.main().goto_mainshoppingflow().MainShoppingFlow()
