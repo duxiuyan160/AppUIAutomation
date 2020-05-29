6857251505f5c7f1aa2958e657c68bbdccf54804
duxiuyan <duxiuyan@126.com>
2020-05-29 13:02:54 +0800

    引用装饰器

--- page/base_page.py
+++ page/base_page.py
@@ -7,6 +7,8 @@ from appium.webdriver.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from appium.webdriver.common.mobileby import MobileBy
 
+from page.wrapper import handle_black
+
 
 class BasePage:
     _driver: WebDriver = None
@@ -23,46 +25,16 @@ class BasePage:
         self._driver = driver
 
     # 通过find_element查找元素
+    @handle_black
     def find(self, locator, value):
-        try:
-            element = self._driver.find_element(locator, value)
-            self._driver.implicitly_wait(10)
-            return element
-        except Exception as e:
-            # 出现异常，将隐式等待设置小一点
-            self._driver.implicitly_wait(1)
-            # 判断异常处理次数
-            if self._error_num > self._max_num:
-                raise e
-            self._error_num += 1
-            for black in self._back_list:
-                elements = self._driver.find_elements(*black)
-                if len(elements) > 0:
-                    elements[0].click()
-                    break
-            # 处理完黑名单后，再次找原来的元素
-            return self.find(locator, value)
+        element = self._driver.find_element(locator, value)
+        return element
 
     # 通过finds_element查找元素
+    @handle_black
     def finds(self, locator, value):
-        try:
-            element = self._driver.find_elements(locator, value)
-            self._driver.implicitly_wait(10)
-            return element
-        except Exception as e:
-            # 出现异常，将隐式等待设置小一点
-            self._driver.implicitly_wait(1)
-        # 判断异常处理次数
-            if self._error_num > self._max_num:
-                raise e
-            self._error_num += 1
-            for black in self._back_list:
-                elements = self._driver.find_elements(*black)
-                if len(elements) > 0:
-                    elements[0].click()
-                    break
-            # 处理完黑名单后，再次找原来的元素
-            return self.finds(locator, value)
+        element = self._driver.find_elements(locator, value)
+        return element
 
     # 点击事件封装
     def click(self, element: WebElement):
@@ -77,12 +49,14 @@ class BasePage:
         with open(filename, encoding="utf8") as f:
             self.datas = yaml.full_load(f)
             return self.datas[section][option1]
+
     # 滑屏
     def do_Scroll(self, value):
         self._driver.find_element_by_android_uiautomator('new UiScrollable('
-                                                            'new UiSelector().scrollable(true).instance(0))'
-                                                            '.scrollIntoView('
-                                                            'new UiSelector().text(' + '\"' + str(value) + '\"' + ').instance(0));').click()
+                                                         'new UiSelector().scrollable(true).instance(0))'
+                                                         '.scrollIntoView('
+                                                         'new UiSelector().text(' + '\"' + str(
+            value) + '\"' + ').instance(0));').click()
 
     # 测试步骤封装#todoß
     def steps(self, path):
